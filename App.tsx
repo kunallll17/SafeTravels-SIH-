e610200796dfe9ee31ac212ea76f3a7c364ac814
kunallll17 <120559248+kunallll17@users.noreply.github.com>
2025-09-08 12:38:31 +0530

    chore(app): configure StatusBar and dependencies for UI overhaul

--- App.tsx
+++ App.tsx
@@ -1,20 +1,12 @@
+import React from 'react';
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { TouristApp } from './components/tourist/TouristApp';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <>
+      <StatusBar style="dark" backgroundColor="#F8FAFC" translucent={false} />
+      <TouristApp />
+    </>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
